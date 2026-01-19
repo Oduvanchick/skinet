81083e4a94e16a89c0fe16ec8ae39f871cc060bf
Natalia Vasylenko <mail.to.nata.vas@gmail.com>
2026-01-19 13:41:08 +0200

    feat: update angular from 19 to 20

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/products';
+import { Pagination } from './shared/models/pagination';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  privatehttp = inject(HttpClient);
+
+  title = 'Skinet';
+  products: Product[] = [];
+
+  // make http request:
+
+  ngOnInit(): void {
+    this.privatehttp.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+
+    });
+
+  }
+
+  //constructor(private http: HttpClient) {}
+}
