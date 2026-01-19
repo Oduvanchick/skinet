ea0edfa6dfc08b7de303eaa4f3c8078d5acd9436
Natalia Vasylenko <mail.to.nata.vas@gmail.com>
2026-01-19 16:14:09 +0200

    feat: add filtering, searching and pagination

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,32 +4,16 @@ import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/products';
 import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  privatehttp = inject(HttpClient);
-
-  title = 'Skinet';
-  products: Product[] = [];
-
-  // make http request:
-
-  ngOnInit(): void {
-    this.privatehttp.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-
-    });
-
-  }
-
-  //constructor(private http: HttpClient) {}
+export class AppComponent {
+  title = 'Skinet'
 }
---
+++ client/src/app/shared/models/shopParams.ts
@@ -0,0 +1,8 @@
+export class ShopParams {
+  brands: string[] = [];
+  types: string[] = [];
+  sort = 'name';
+  pageSize = 10;
+  pageNumber = 1;
+  search = '';
+}
