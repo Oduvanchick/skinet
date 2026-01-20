f8c7dfddba3669cd87c5289e1e771f24d0718eb7
Natalia Vasylenko <mail.to.nata.vas@gmail.com>
2026-01-20 20:03:16 +0200

    feat: implement routing

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81083e4a94e16a89c0fe16ec8ae39f871cc060bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0edfa6dfc08b7de303eaa4f3c8078d5acd9436")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
